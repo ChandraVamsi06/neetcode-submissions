86fbceabca8ae900d99dfe65fb01e6c87f171f7e
Sana Chandra Vamsi <147473020+ChandraVamsi06@users.noreply.github.com>
2026-06-19 14:26:07 +0530

    Add: longest-common-prefix - submission-0

---
+++ Data Structures & Algorithms/longest-common-prefix/submission-0.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        string res = strs[0];
+        for(int i = 1; i < strs.size(); i++) {
+            while(strs[i].find(res) != 0) {
+                res.pop_back();
+            }
+        }
+        return res;
+    }
+};
